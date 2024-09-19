cba68a168293ef38ceee76afdfe037f2ca8142b2
Paulo Jr <pauloricardo.jr1@gmail.com>
2024-09-19 01:29:22 -0300

    refactor: added repository annotation in UserJpaRepository.java

--- infrastructure/src/main/java/me/paulojr/cadastro/infra/context/user/repository/UserJpaRepository.java
+++ infrastructure/src/main/java/me/paulojr/cadastro/infra/context/user/repository/UserJpaRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserJpaRepository extends JpaRepository<UserJpaEntity, Long> {
     Page<UserJpaEntity> findAll(Specification<UserJpaEntity> where, Pageable pageable);
 }
