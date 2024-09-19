f7d0211b0e385fefa36eeeb6a56ede0d69a260da
Paulo Jr <pauloricardo.jr1@gmail.com>
2024-09-19 01:58:21 -0300

    feat: remove unused imports

--- infrastructure/src/main/java/me/paulojr/cadastro/infra/config/WebServerConfig.java
+++ infrastructure/src/main/java/me/paulojr/cadastro/infra/config/WebServerConfig.java
@@ -1,17 +1,10 @@
 package me.paulojr.cadastro.infra.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @ComponentScan("me.paulojr.cadastro.infra")
