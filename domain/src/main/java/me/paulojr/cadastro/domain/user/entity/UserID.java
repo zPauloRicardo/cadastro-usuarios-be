d07fff6b434f145b855e42bc2092b8f7e132f0d9
Paulo Jr <pauloricardo.jr1@gmail.com>
2024-09-19 01:06:17 -0300

    refactor: throw error when id value is null on constructor

--- domain/src/main/java/me/paulojr/cadastro/domain/user/entity/UserID.java
+++ domain/src/main/java/me/paulojr/cadastro/domain/user/entity/UserID.java
@@ -2,13 +2,18 @@ package me.paulojr.cadastro.domain.user.entity;
 
 import me.paulojr.cadastro.domain.shared.domain.Identifier;
 
+import java.util.Objects;
+
 public class UserID extends Identifier<Long> {
 
     private final Long id;
 
     public UserID(Long id) {
-        this.id = id;
+        this.id = Objects.requireNonNull(id, "ID inválido.");
     }
+
+
+
 
     @Override
     public Long getValue() {
