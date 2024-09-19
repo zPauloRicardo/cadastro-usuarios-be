3922db957f9e3b29121034b4d782903489c44406
Paulo Jr <pauloricardo.jr1@gmail.com>
2024-09-19 01:59:35 -0300

    fix: bean UserJpaGateway

--- infrastructure/src/main/java/me/paulojr/cadastro/infra/context/user/UserJpaGateway.java
+++ infrastructure/src/main/java/me/paulojr/cadastro/infra/context/user/UserJpaGateway.java
@@ -16,9 +16,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class UserJpaGateway implements UserGateway {
 
